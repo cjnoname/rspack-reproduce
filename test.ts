f56ddbe6ddd40d00bb84f1206509aef0ca696ad8
CJ <powerful_j@outlook.com>
2024-09-30 09:57:05 +1000

    fix

--- test.ts
+++ test.ts
@@ -3,13 +3,13 @@ import type {
     ConnectionOptions,
     SnowflakeError
   } from "snowflake-sdk";
-  import { createConnection } from "snowflake-sdk";
-  
- export const getSnowflakeConnection = (connection: ConnectionOptions): Promise<Connection> => {
+import { createConnection } from "snowflake-sdk";
+
+export const getSnowflakeConnection = (connection: ConnectionOptions): Promise<Connection> => {
     return new Promise((resolve, reject) =>
-      createConnection(connection).connect((err: SnowflakeError | undefined, conn: Connection) =>
+        createConnection(connection).connect((err: SnowflakeError | undefined, conn: Connection) =>
         err ? reject(err) : resolve(conn)
-      )
+        )
     );
-  };
+};
   
