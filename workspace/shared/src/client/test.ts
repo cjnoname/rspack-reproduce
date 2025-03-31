53d732231441c54248367fe38fb65b48ad69b571
CJ <powerful_j@outlook.com>
2025-03-31 16:06:25 +1100

    add comment

--- workspace/shared/src/client/test.ts
+++ workspace/shared/src/client/test.ts
@@ -1,3 +1,5 @@
+// Rspack should recognize the "@shared" alias from tsconfig and resolve it during the build step.
+
 import { BBB } from "@shared/src/client/test2";
 
 export const AAA = "Yes I am AAA";
