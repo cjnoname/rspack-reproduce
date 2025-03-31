6893a7c7272c94151b00849464ee398df17d1b9e
CJ <powerful_j@outlook.com>
2025-03-31 16:10:44 +1100

    fix

--- workspace/shared/src/client/test.ts
+++ workspace/shared/src/client/test.ts
@@ -1,4 +1,5 @@
-// Rspack should recognize the "@shared" alias from tsconfig and resolve it during the build step.
+// HERE!!! Rspack should recognize the "@shared" alias from tsconfig and resolve it automatically during the build step,
+// instead of using the "resolve.alias" option.
 
 import { BBB } from "@shared/src/client/test2";
 
